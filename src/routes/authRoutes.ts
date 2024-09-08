c2d329cf7ec1a1f06a4a7729640980c59c17988e
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-09-08 16:33:45 +0300

    Formatted routes for easier reading

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,17 +4,23 @@ import { methodNotAllowed } from '../controllers/suspicionController';
 import { registerValidation, loginValidation } from '../validators/authFieldsValidation';
 import validateJoiRequest from '../middlewares/validateJoiRequest';
 import authMiddleware from '../middlewares/authMiddleware';
-import adminMiddleware from '../middlewares/adminMiddleware';
 
 const authRouter = Router();
 
-authRouter.route('/signup').post(validateJoiRequest({ bodySchema: registerValidation }), signup);
-authRouter.route('/login').post(validateJoiRequest({ bodySchema: loginValidation }), login);
-authRouter.route('/logout').get(authMiddleware, logout);
-
-authRouter.route('/protected').get(authMiddleware, adminMiddleware, (req, res) => {
-  res.send('Hello, authenticated user!');
-});
+authRouter.route('/signup')
+  .post(
+    validateJoiRequest({ bodySchema: registerValidation }), 
+    signup,
+  );
+authRouter.route('/login')
+  .post(validateJoiRequest({ bodySchema: loginValidation }),
+  login,
+  );
+authRouter.route('/logout')
+.get(
+  authMiddleware, 
+  logout,
+);
 
 authRouter.route('*').all(methodNotAllowed);
 
