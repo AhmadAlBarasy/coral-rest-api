40f0470ed68ec9ce112fa976e2fd63e99e5a6bd5
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-09-26 18:30:30 +0300

    Refactored auth fields validation further more

--- src/schemas/authFieldsValidation.ts
+++ src/schemas/authFieldsValidation.ts
@@ -1,36 +1,5 @@
 import Joi, { StringSchema } from 'joi';
-
-const passwordValidator: StringSchema = Joi.string()
-  .min(8)
-  .required()
-  .pattern(/[A-Z]/, 'uppercase letter')
-  .pattern(/[a-z]/, 'lowercase letter')
-  .pattern(/\d/, 'number')
-  .pattern(/[\W_]/, 'special character')
-  .messages({
-    'string.min': 'Password must be at least 8 characters long',
-    'string.pattern.name': 'Password must contain at least one {#name}',
-    'any.required': 'Password is required',
-});
-
-const emailValidator: StringSchema = Joi.string()
-  .email({ tlds: { allow: false } })
-  .required()
-  .messages({
-    'string.email': 'Invalid email format',
-    'any.required': 'Email is required',
-});
-
-const nameValidator = (order: string): StringSchema => Joi.string()
-  .min(3)
-  .max(20)
-  .required()
-  .messages({
-    'string.base': `${order} name must be a string`,
-    'string.min': `${order} name must be at least 3 characters long`,
-    'string.max': `${order} name must be less than or equal to 20 characters long`,
-    'any.required': `${order} name is required`,
-});
+import { emailValidator, passwordValidator, stringFieldValidator } from './validators';
 
 const forgotPasswordValidation = Joi.object({
   email: emailValidator,
@@ -46,10 +15,10 @@ const loginValidation = Joi.object({
 });
 
 const registerValidation = Joi.object({
-  firstName: nameValidator('First'),
-  lastName: nameValidator('Last'),
+  firstName: stringFieldValidator('First name', 20, 3),
+  lastName: stringFieldValidator('Last name', 20, 3),
   email: emailValidator,
-  password:passwordValidator,
+  password: passwordValidator,
 });
 
 export { 
