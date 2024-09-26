cfb2a6c76c29e73b80603c2513557800a88a120d
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-09-26 15:16:15 +0300

    Fixed logger format and coloring

--- src/loggers/app-logger.ts
+++ src/loggers/app-logger.ts
@@ -1,14 +1,21 @@
 import winston, { Logger, format } from 'winston';
+import { greenBright, magenta } from 'colorette';
 
-const { json, cli, timestamp, combine } = format;
+const { json, printf, timestamp, combine } = format;
+
+const cliLoggerFormat =  printf(({level, message, timestamp}) => {
+  return `[${magenta(timestamp)}] ${greenBright(level)}: ${message}`;
+});
 
 const logger: Logger = winston.createLogger({
   // for production, you might not need for example 'silly' level to be used,
   // so you specify the minimum level to be logged in you environment variables.
   level: process.env.LOG_LEVEL || 'http',
-  format: combine(timestamp(), cli()), // time in UTC standard
-  transports: [
-    new winston.transports.Console()],
+  format: combine(
+    timestamp(),
+    cliLoggerFormat, // time in UTC standard
+  ),
+  transports: [new winston.transports.Console()],
 });
 
 const fileLogger: Logger = winston.createLogger({
