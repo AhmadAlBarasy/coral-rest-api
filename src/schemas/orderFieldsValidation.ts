d05f291dda5c18727e730be67d2c1d9895cab911
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-09-26 18:31:54 +0300

    Refactored order fields validation code

--- src/schemas/orderFieldsValidation.ts
+++ src/schemas/orderFieldsValidation.ts
@@ -1,49 +1,22 @@
 /* eslint-disable max-len */
 import Joi from 'joi';
+import { stringFieldValidator, uuidV4validator } from './validators';
 
 const addressValidation = Joi.object({
-  state: Joi.string()
-    .max(50)
-    .required()
-    .messages({
-      'any.required': 'The state field is required. Please provide the name of the state where the delivery should be made.',
-      'string.base': 'The state must be a text string. Please ensure it is a valid state name.',
-      'string.max': 'The state name must not exceed 50 characters. Please provide a shorter state name.',
-    }),
-  city: Joi.string()
-    .max(50)
-    .required()
-    .messages({
-      'any.required': 'The city field is required. Please provide the name of the city where the delivery should be made.',
-      'string.base': 'The city must be a text string. Please ensure it is a valid city name.',
-      'string.max': 'The city name must not exceed 50 characters. Please provide a shorter city name.',
-    }),
-  street: Joi.string()
-    .max(50)
-    .required()
-    .messages({
-      'any.required': 'The street field is required. Please provide the street name or number for the delivery address.',
-      'string.base': 'The street must be a text string. Please ensure it is a valid street name.',
-      'string.max': 'The street name must not exceed 50 characters. Please provide a shorter street name.',
-    }),
+  state: stringFieldValidator('State', 50, 3),
+  city: stringFieldValidator('City', 50, 3),
+  street: stringFieldValidator('Street', 50, 3),
   pin: Joi.string()
     .pattern(/^\d+$/)
     .required()
     .messages({
-      'any.required': 'The PIN code is required. Please provide the postal code for the delivery address.',
+      'any.required': 'The PIN code is required.',
       'string.pattern.base': 'The PIN code must contain only digits. Please enter a valid numerical postal code.',
     }),
 });
 
 const orderItemValidation = Joi.object({
-  id: Joi.string()
-    .uuid({ version: 'uuidv4' })
-    .required()
-    .messages({
-      'string.guid': 'Product ID must be a valid UUID.',
-      'any.required': 'Product ID is required',
-      'string.base': 'Product ID must be a string',
-    }),
+  id: uuidV4validator('Product ID'),
   quantity: Joi.number()
     .integer()
     .min(1)
@@ -68,7 +41,7 @@ const createOrderValidation = Joi.object({
   address: addressValidation
     .required()
     .messages({
-      'any.required': 'Address information is required. Please provide a complete address including state, city, street, and PIN code.',
+      'any.required': 'Address information is required.',
       'object.base': 'The address must be an object containing state, city, street, and PIN code.',
     }),
   phoneNumber: Joi.string()
@@ -78,16 +51,7 @@ const createOrderValidation = Joi.object({
       'any.required': 'Phone number is required. Please provide a valid phone number.',
       'string.pattern.base': 'Phone number must start with a "+" sign and be followed by 10 to 15 digits. Ensure the number is in the correct international format.',
     }),
-  orderOwner: Joi.string()
-    .min(3)
-    .max(50)
-    .required()
-    .messages({
-      'string.base': 'Order Owner name must be a string',
-      'string.min': 'Order Owner name must be at least 3 characters long',
-      'string.max': 'Order Owner name must be less than or equal to 50 characters long',
-      'any.required': 'Order Owner name is required',
-    }),
+  orderOwner: stringFieldValidator('Order owner', 50, 3),
   cardNumber: Joi.string()
     .required()
     .pattern(
@@ -100,13 +64,6 @@ const createOrderValidation = Joi.object({
     }),
 });
 const orderIdValidation = Joi.object({
-  id: Joi.string()
-    .uuid({ version: 'uuidv4' })
-    .required()
-    .messages({
-      'string.guid': 'order ID must be a valid UUID.',
-      'any.required': 'order ID is required',
-      'string.base': 'order ID must be a string',
-    }),
+  id: uuidV4validator('Order ID'),
 });
 export { createOrderValidation, orderIdValidation };
