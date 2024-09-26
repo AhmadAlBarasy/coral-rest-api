8fd8b5ac03250fd01b9294da73dfc7e88d3da53d
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-09-26 18:31:24 +0300

    Refactored category fields validation further more

--- src/schemas/categoryFieldsValidation.ts
+++ src/schemas/categoryFieldsValidation.ts
@@ -1,45 +1,14 @@
 import Joi from 'joi';
-
-const categoryNameValidator = Joi.string()
-  .min(3)
-  .max(50)
-  .required()
-  .messages({
-    'string.base': 'Name should be a type of text.',
-    'string.empty': 'Name cannot be empty.',
-    'string.max': 'Name should have a maximum length of {#limit}.',
-    'any.required': 'Name is a required field.',
-  });
-
-const categoryDescriptionValidator = Joi.string()
-  .max(150)
-  .required()
-  .messages({
-    'string.base': 'Description should be a type of text.',
-    'string.empty': 'Description cannot be empty.',
-    'string.max': 'Description should have a maximum length of {#limit}.',
-    'any.required': 'Description is a required field.',
-  });
-
-const categoryIdValidation = Joi.object({
-  id: Joi.string()
-    .uuid({ version: 'uuidv4' })
-    .required()
-    .messages({
-      'string.base': 'ID should be a type of string.',
-      'string.guid': 'ID must be a valid UUID.',
-      'any.required': 'ID is a required field.',
-    }),
-});
+import { stringFieldValidator } from './validators';
 
 const createCategoryValidation = Joi.object({
-  name: categoryNameValidator,
-  description: categoryDescriptionValidator,
+  name: stringFieldValidator('Category Name', 50, 3),
+  description: stringFieldValidator('Category description', 150, 3),
 });
 
 const updateCategoryValidation = Joi.object({
-  name: categoryNameValidator,
-  description: categoryDescriptionValidator,
+  name: stringFieldValidator('Category Name', 50, 3),
+  description: stringFieldValidator('Category description', 150, 3),
 });
 
-export  { createCategoryValidation , categoryIdValidation ,updateCategoryValidation };
+export  { createCategoryValidation, updateCategoryValidation };
